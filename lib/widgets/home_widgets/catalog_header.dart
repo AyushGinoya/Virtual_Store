d8ac559497c98be1f92fd1cf7bc88dff2ccf37c6
Ayush <124197069+AyushGinoya@users.noreply.github.com>
2023-06-11 23:33:34 +0530

    Update catalog_header.dart

--- lib/widgets/home_widgets/catalog_header.dart
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -3,13 +3,15 @@ import 'package:virtual_store/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
-        "Trending products".text.xl2.make(),
+        "Virtual Store".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Top-rated Products".text.xl2.make(),
       ],
     );
   }
