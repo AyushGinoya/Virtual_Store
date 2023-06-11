0d680ac4f4524c560857a2a51766bc323a9bd8f8
Ayush <124197069+AyushGinoya@users.noreply.github.com>
2023-06-11 23:34:18 +0530

    Update drawer.dart

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -2,22 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final imageUrl =
+    const imageUrl =
         "https://avatars.githubusercontent.com/u/12619420?s=460&u=26db98cbde1dd34c7c67b85c240505a436b2c36d&v=4";
     return Drawer(
       child: Container(
         color: Colors.deepPurple,
         child: ListView(
           padding: EdgeInsets.zero,
-          children: [
+          children: const [
             DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
                 margin: EdgeInsets.zero,
-                accountName: Text("Pawan Kumar"),
-                accountEmail: Text("mtechviral@gmail.com"),
+                accountName: Text("Ayush Ginoya"),
+                accountEmail: Text("ginoyaayush@gmail.com"),
                 currentAccountPicture: CircleAvatar(
                   backgroundImage: NetworkImage(imageUrl),
                 ),
